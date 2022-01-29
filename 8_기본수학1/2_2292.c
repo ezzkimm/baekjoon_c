381e604b37cff1c2af199fb73347d98cda201701
ezzkimm <dmswjd1515@gmail.com>
2022-01-30 01:36:31 +0900

    21.01.30

--- 8_기본수학1/2_2292.c
+++ 8_기본수학1/2_2292.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int n;
     scanf("%d", &n);
     int step = 1;
-    while (1){
-        if (n <=1) break;
-        n -= step*6;
+    while (1)
+    {
+        if (n <= 1)
+            break;
+        n -= step * 6;
         step++;
     }
     printf("%d", step);
